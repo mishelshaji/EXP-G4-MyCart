e9e1a6ae74d0f7ab1ab7ca15ddb166c771ed0659
princegeorge121 <89679612+princegeorge121@users.noreply.github.com>
2023-02-07 12:43:13 +0530

    Update app module.ts

--- client/mycart/src/app/app.module.ts
+++ client/mycart/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FeedbackComponent } from './feedback/feedback.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FeedbackComponent
   ],
   imports: [
     BrowserModule,
