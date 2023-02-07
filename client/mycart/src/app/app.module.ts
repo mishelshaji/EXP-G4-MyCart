c20191208b7b7cc31ce9006a606029c3fe445048
princegeorge121 <89679612+princegeorge121@users.noreply.github.com>
2023-02-07 13:05:10 +0530

    Updated footer component and added font awesome packages

--- client/mycart/src/app/app.module.ts
+++ client/mycart/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/mycart/src/app/shared/footer/footer.component.ts
+++ client/mycart/src/app/shared/footer/footer.component.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import {  faShopify} from "@fortawesome/free-brands-svg-icons";
+import { faShopify, faFacebook, faTwitter, faWhatsapp, faInstagram, faLinkedin, faGit } from "@fortawesome/free-brands-svg-icons";
 import { RouterModule } from "@angular/router";
 
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule,RouterModule,FontAwesomeModule],
+  imports: [CommonModule, RouterModule, FontAwesomeModule],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
-  faShop=faShopify;
+  faShop = faShopify;
+  faFb = faFacebook;
+  faTw = faTwitter;
+  faWtsapp = faWhatsapp;
+  faInsta = faInstagram;
+  faLin = faLinkedin;
+  faGit = faGit
+
+
 }
