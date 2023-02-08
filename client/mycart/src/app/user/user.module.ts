4cb4078f5ca97fee274b8826f02ab1f5c81d213b
MUHAMMED K A <103103686+Muhammed-KA@users.noreply.github.com>
2023-02-08 09:59:42 +0530

    Cart design completed

--- client/mycart/src/app/user/user.module.ts
+++ client/mycart/src/app/user/user.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { CartItemComponent } from '../customer/shared/cart-item/cart-item.component';
-import { CartComponent } from '../customer/cart/cart.component';
 import { UserRoutingModule } from './user-routing.module';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { UserLayoutComponent } from './user-layout/user-layout.component';
