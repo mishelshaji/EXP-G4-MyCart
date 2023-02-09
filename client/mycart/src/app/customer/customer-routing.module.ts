4ee9669a7c8a7af3a5eb72ba5a8e2b40a8368e40
princegeorge121 <89679612+princegeorge121@users.noreply.github.com>
2023-02-09 13:23:20 +0530

    updated footer component

--- client/mycart/src/app/customer/customer-routing.module.ts
+++ client/mycart/src/app/customer/customer-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { CartComponent } from './cart/cart.component';
 import { CustomerHomeComponent } from './customer-home/customer-home.component';
 import { CustomerLayoutComponent } from './customer-layout/customer-layout.component';
+import { FeedbackComponent } from './feedback/feedback.component';
 
 const routes: Routes = [
   {
     path: '', component: CustomerLayoutComponent, children: [
         { path: 'cart', component: CartComponent },
-        { path: 'customerHome', component: CustomerHomeComponent }
+        { path: 'customerHome', component: CustomerHomeComponent },
+        { path: 'feedback' , component: FeedbackComponent}
     ]
   }
 ];
