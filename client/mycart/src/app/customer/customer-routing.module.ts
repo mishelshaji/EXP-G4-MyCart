9239321fdc6718dbb039a5271f027cd6888e1333
princegeorge121 <89679612+princegeorge121@users.noreply.github.com>
2023-02-10 17:49:16 +0530

    Update customer-routing.module.ts

--- client/mycart/src/app/customer/customer-routing.module.ts
+++ client/mycart/src/app/customer/customer-routing.module.ts
@@ -4,6 +4,7 @@ import { HomePageComponent } from '../shared/home-page/home-page.component';
 import { CartComponent } from './cart/cart.component';
 import { CustomerHomeComponent } from './customer-home/customer-home.component';
 import { CustomerLayoutComponent } from './customer-layout/customer-layout.component';
+import { FeedbackComponent } from './feedback/feedback.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 
 const routes: Routes = [
