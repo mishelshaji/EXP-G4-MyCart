c37cd668480b9a549068a7b33538bcb75b1f8d17
princegeorge121 <89679612+princegeorge121@users.noreply.github.com>
2023-02-10 15:01:25 +0530

    Update customer-routing.module.ts

--- client/mycart/src/app/customer/customer-routing.module.ts
+++ client/mycart/src/app/customer/customer-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
   {
     path: '', component: CustomerLayoutComponent, children: [
         { path: 'cart', component: CartComponent },
-        { path: 'customerHome', component: CustomerHomeComponent },
+        { path: 'home', component: CustomerHomeComponent },
         { path: 'feedback' , component: FeedbackComponent},
     ]
   }
