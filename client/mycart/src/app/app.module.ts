f738af5358c7d8e7ee9506ae287a81b27668b134
princegeorge121 <89679612+princegeorge121@users.noreply.github.com>
2023-02-10 10:50:37 +0530

    Update app.module.ts

--- client/mycart/src/app/app.module.ts
+++ client/mycart/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CustomerProfileComponent } from './customer-profile/customer-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CustomerProfileComponent,
   ],
   imports: [
     BrowserModule,
