d3e41ee15714ac0fcb25de19ba3061c0d83137c2
MUHAMMED K A <103103686+Muhammed-KA@users.noreply.github.com>
2023-02-13 15:00:22 +0530

    invoice completed

--- client/mycart/src/app/customer/customer-routing.module.ts
+++ client/mycart/src/app/customer/customer-routing.module.ts
@@ -4,6 +4,8 @@ import { HomePageComponent } from '../shared/home-page/home-page.component';
 import { CartComponent } from './cart/cart.component';
 import { CustomerHomeComponent } from './customer-home/customer-home.component';
 import { CustomerLayoutComponent } from './customer-layout/customer-layout.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { InvoiceComponent } from './invoice/invoice.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 
 const routes: Routes = [
@@ -13,6 +15,7 @@ const routes: Routes = [
       { path: 'home', component: CustomerHomeComponent },
       { path: 'order-summary', component: OrderSummaryComponent },
       { path: 'feedback' , component: FeedbackComponent},
+      { path: 'invoice' , component: InvoiceComponent}
     ]
   }
 ];
