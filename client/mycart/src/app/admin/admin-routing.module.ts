381f7bf69519c243d9c5d04afcbc03218711f1a7
princegeorge121 <89679612+princegeorge121@users.noreply.github.com>
2023-02-14 15:00:22 +0530

    Created Update Product component

--- client/mycart/src/app/admin/admin-routing.module.ts
+++ client/mycart/src/app/admin/admin-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddProductComponent } from './add-product/add-product.component';
 import { AdminLayoutComponent } from './admin-layout/admin-layout.component';
+import { UpdateProductComponent } from './update-product/update-product.component';
 import { ViewFeedbackComponent } from './view-feedback/view-feedback.component';
 
 const routes: Routes = [
   {
     path: '', component: AdminLayoutComponent, children: [
       { path: 'product/create', component: AddProductComponent },
-      { path: 'feedback/view', component: ViewFeedbackComponent }
+      { path: 'feedback/view', component: ViewFeedbackComponent },
+      { path: 'product/update', component:UpdateProductComponent}
     ]
   }
 ];
